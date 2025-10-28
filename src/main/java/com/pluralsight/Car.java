d8e52894107ef0697dab20dfd1615093ad0a9271
samnam298 <samrawithailena@gmail.com>
2025-10-28 10:13:57 -0400

    class extends Vehicle — it inherits all its data and adds its own

--- src/main/java/com/pluralsight/Car.java
+++ src/main/java/com/pluralsight/Car.java
@@ -1,4 +1,16 @@
 package com.pluralsight;
 
-public class Car {
+public class Car extends Vehicle {
+    private int doors;
+
+    // Child constructor that calls parent constructor
+    public Car(String make, String model, int year, int doors) {
+        super(make, model, year);  // calls Vehicle’s constructor
+        this.doors = doors;
+    }
+
+    public void displayCarInfo() {
+        displayInfo(); // from Vehicle
+        System.out.println("Doors: " + doors);
+    }
 }
